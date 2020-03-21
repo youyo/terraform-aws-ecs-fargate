694b452518c6e9d803c4b0419511c91695aed25b
youyo <1003ni2@gmail.com>
2020-03-21 13:25:17 +0900

    default_capacity_provider_strategy is use for_each

--- main.tf
+++ main.tf
@@ -2,10 +2,14 @@ resource "aws_ecs_cluster" "this" {
   name               = var.name
   capacity_providers = var.capacity_providers
 
-  default_capacity_provider_strategy {
-    capacity_provider = var.default_capacity_provider_strategy_capacity_provider
-    base              = var.default_capacity_provider_strategy_base
-    weight            = var.default_capacity_provider_strategy_weight
+  dynamic "default_capacity_provider_strategy" {
+    for_each = var.default_capacity_provider_strategy
+
+    content {
+      capacity_provider = default_capacity_provider_strategy.value["capacity_provider"]
+      base              = default_capacity_provider_strategy.value["base"]
+      weight            = default_capacity_provider_strategy.value["weight"]
+    }
   }
 
   setting {
--- variables.tf
+++ variables.tf
@@ -13,20 +13,21 @@ variable "enable_container_insights" {
   default     = true
   description = "Enable CloudWatch Container Insights for a cluster"
 }
-variable "default_capacity_provider_strategy_capacity_provider" {
-  type        = string
-  default     = "FARGATE_SPOT"
-  description = "The short name of the capacity provider"
-}
-variable "default_capacity_provider_strategy_base" {
-  type        = number
-  default     = 0
-  description = "The number of tasks, at a minimum, to run on the specified capacity provider"
-}
-variable "default_capacity_provider_strategy_weight" {
-  type        = number
-  default     = 1
-  description = "The relative percentage of the total number of launched tasks that should use the specified capacity provider"
+variable "default_capacity_provider_strategy" {
+  type        = list(map(string))
+  description = "The capacity provider strategy to use by default for the cluster"
+  default = [
+    {
+      capacity_provider = "FARGATE"
+      base              = 1
+      weight            = 1
+    },
+    {
+      capacity_provider = "FARGATE_SPOT"
+      base              = 0
+      weight            = 1
+    }
+  ]
 }
 variable "tags" {
   type        = map(string)
